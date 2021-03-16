656a174a6dba96d457c14c302885a6e1bed3b583
DMITRIY YUSHIN <49056603+DmitrySar@users.noreply.github.com>
2021-03-16 12:06:17 +0300

    camera follows the hero

---
+++ Assets/scripts/Camera.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private float speed = 3f;
+    private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+        Vector3 position = target.transform.position;
+        transform.position = new Vector3(position.x, position.y, transform.position.z);
+        print(GameObject.Find("Player").name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = target.position;
+        position.z = transform.position.z;
+        transform.position = Vector3.Lerp(transform.position, position, speed * Time.deltaTime);
+    }
+}
